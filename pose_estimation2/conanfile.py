f7f6a6a0f54ed863fab96dde4a52a426589b3669
memsharded <james@conan.io>
2023-11-10 21:18:30 +0100

    wip

--- pose_estimation2/conanfile.py
+++ pose_estimation2/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import cmake_layout
 
 class PoseEstimationRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires("tensorflow-lite/2.12.0")
